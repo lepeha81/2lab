6b241fff2ae4cf465d9e88aa6e06373767999d14
lepeha81 <112777048+lepeha81@users.noreply.github.com>
2023-03-08 18:10:03 +0300

    Update Helloworld.cpp

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -4,7 +4,7 @@
 int
 main(int argc, char* argv)
 {
-  std::string name; // используем переменную типа string.....
+  std::string name; // 
   std::cout << "Input user_name" : std::cin >>
     name; // .....вводим своё имя в переменную....
   std::cout << "Hello world from " << name << std::endl; // выводим приветствие
