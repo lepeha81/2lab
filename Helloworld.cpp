def6c5937dd05488560553aeeafdffc458fb26c4
lepeha <alex-lobnya-2005@mail.ru>
2023-03-08 17:25:40 +0300

    Шестое изменение

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char* argv){
-std::string name; // используем переменную типа string
-std::cout << "Input user_name":
-std::cin >> name; // вводим своё имя в переменную
-std::cout << "Hello world from " << name << std::endl; // выводим приветствие
-return 0;
+int
+main(int argc, char* argv)
+{
+  std::string name; // используем переменную типа string
+  std::cout << "Input user_name" : std::cin >>
+    name; // вводим своё имя в переменную
+  std::cout << "Hello world from " << name << std::endl; // выводим приветствие
+  return 0;
 }
