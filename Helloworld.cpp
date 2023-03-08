0edd1e4b41bdeaca562fdadd2250918c74ee1779
lepeha <alex-lobnya-2005@mail.ru>
2023-03-08 17:54:51 +0300

    4 пункт

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -6,7 +6,7 @@ main(int argc, char* argv)
 {
   std::string name; // используем переменную типа string
   std::cout << "Input user_name" : std::cin >>
-    name; // вводим своё имя в переменную
+    name; // .....вводим своё имя в переменную
   std::cout << "Hello world from " << name << std::endl; // выводим приветствие
   return 0;
 }
