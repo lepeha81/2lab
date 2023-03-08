5071e2dc4d70a41c4a65bc9ce68efc45e1a789d1
lepeha <alex-lobnya-2005@mail.ru>
2023-03-08 18:47:37 +0300

    e

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -4,9 +4,9 @@
 int
 main(int argc, char* argv)
 {
-  std::string name; // используем переменную типа string
+  std::string name; //используем переменную типа string
   std::cout << "Input user_name" : std::cin >>
-    name; // .....вводим своё имя в переменную
+    name; // вводим своё имя в переменную
   std::cout << "Hello world from " << name << std::endl; // выводим приветствие
   return 0;
 }
